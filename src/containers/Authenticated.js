21ae777ba11cc06cbee6999bf4d490f55aa5fce7
Ying Cong <yingcong@glints.com>
2017-05-04 17:00:43 +0800

    block out admin ui

--- src/containers/Authenticated.js
+++ src/containers/Authenticated.js
@@ -3,10 +3,11 @@ import { connect } from 'react-redux';
 import { browserHistory } from 'react-router';
 import jwt from 'jwt-decode';
 
-export default function authenticate(WrappedComponent, optional = false) {
+export default function authenticate(WrappedComponent) {
   class AuthenticatedComponent extends Component {
     static propTypes = {
       authenticated: React.PropTypes.bool.isRequired,
+      credentials: React.PropTypes.object,
       location: React.PropTypes.shape({
         pathname: React.PropTypes.string,
         search: React.PropTypes.string
@@ -19,7 +20,7 @@ export default function authenticate(WrappedComponent, optional = false) {
       this.checkAuthenticated();
     }
     checkAuthenticated() {
-      if (!this.props.authenticated && !optional) {
+      if (!this.props.authenticated) {
         const locationSearch = this.props.location.search;
         const query = locationSearch.replace('?', '&');
         const redirectAfterLogin = this.props.location.pathname + query;
@@ -27,15 +28,19 @@ export default function authenticate(WrappedComponent, optional = false) {
       }
     }
     render() {
-      if (this.props.authenticated || optional) {
+      if (this.props.authenticated) {
         return <WrappedComponent {...this.props} />;
       }
-
-      return null;
+      return (
+        <div className="container">
+          <h1>Sorry, you are not authorized.</h1>
+        </div>
+      );
     }
   }
 
   return connect(state => ({
-    authenticated: Boolean(state.login && state.login.token && jwt(state.login.token))
+    authenticated: Boolean(state.login && state.login.token && jwt(state.login.token)),
+    credentials: state.login && state.login.token ? jwt(state.login.token) : null,
   }))(AuthenticatedComponent);
 }
