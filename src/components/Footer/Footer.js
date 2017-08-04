652773b5f2557ab64d6160da67257e4f6482f50e
Ying Cong <yingcong@glints.com>
2017-08-05 01:48:01 +0800

    removed powered by glints footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,9 +2,7 @@ import React from 'react';
 import './Footer.scss';
 
 export const Footer = () => (
-  <div className="footer">
-    // <a href="https://glints.com">Powered by Glints</a>
-  </div>
+  <div className="footer" />
 );
 
 export default Footer;
